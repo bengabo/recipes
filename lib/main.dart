8cead42c4f7513ac4298596822624c8c6efb0053
Bengabo <ben@bengabo.com>
2022-02-16 11:39:50 +0100

    assets folder created with photos, recipe.dart class

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
+import 'recipe.dart';
 
 void main() {
-  runApp(RecipeApp());
+  runApp(const RecipesApp());
 }
 
-class RecipeApp extends StatelessWidget {
-  final ThemeData theme = ThemeData();
+class RecipesApp extends StatelessWidget {
+  const RecipesApp({Key? key}) : super(key: key);
 
-  // 1 This widget is the root of your application.
+  // 1
   @override
   Widget build(BuildContext context) {
     // 2
@@ -16,47 +17,83 @@ class RecipeApp extends StatelessWidget {
     return MaterialApp(
       // 4
       title: 'Recipe Calculator',
-      //5
+      // 5
       theme: theme.copyWith(
         colorScheme: theme.colorScheme.copyWith(
-          primary: Colors.grey,
-          secondary: Colors.black
+            primary: Colors.black38,
+            secondary: Colors.orangeAccent
         ),
       ),
-      home: MyHomePage(title: 'Recipe Calculator'),
+      // 6
+      home: const MyHomePage(title: 'Recipe Calculator'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
 
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    // TODO: implement build
+    // 1
     return Scaffold(
+      // 2
       appBar: AppBar(
         title: Text(widget.title),
       ),
+      // 3
       body: SafeArea(
-        child: Container(),
+        // 4
+        child: ListView.builder(
+          // 5
+          itemCount: Recipe.samples.length,
+          // 6
+          itemBuilder: (BuildContext context, int index) {
+            // 7
+            // TODO: Add GestureDetector
+            return buildRecipeCard(Recipe.samples[index]);
+          },
+        ),
       ),
     );
+  }
+
+  Widget buildRecipeCard(Recipe recipe) {
+    return Card(
+      // 1
+      elevation: 2.0,
+      // 2
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(10.0)),
+      // 3
+      child: Padding(
+        padding: const EdgeInsets.all(16.0),
+        // 4
+        child: Column(
+        children: <Widget>[
+          Image(image: AssetImage(recipe.imageUrl)),
+          // 5
+          const SizedBox(
+            height: 14.0,
+          ),
+          // 6
+          Text(
+            recipe.label,
+            style: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.w700,
+              fontFamily: 'Palatine'
+            ),
+          )
+        ],
+      ),
+    ));
   }
 }
